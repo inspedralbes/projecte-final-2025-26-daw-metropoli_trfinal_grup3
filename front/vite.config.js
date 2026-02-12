080d15c714b9abac15fcbbc627a2317e0b89bc95
a23edstorcev <a23edstorcev@inspedralbes.cat>
2026-02-12 18:58:54 +0100

    creacion del docker

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, 
+    watch: {
+        usePolling: true
+    }
+  }
 })
